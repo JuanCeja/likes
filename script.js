c00ea91e475f9a6ad5c537d067efb8cc71bebf4e
Juan Ceja <jmc347ls2@yahoo.com>
2022-02-01 19:13:43 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var count = 9;
+var like = document.querySelector("#likes");
+
+console.log(like);
+
+function addLike(){
+    count++;
+    like.innerText = count + " like(s)";
+    console.log(count);
+}
+
+var count = 12;
+var like1 = document.querySelector("#likes1");
+
+console.log(like);
+
+function addLike1(){
+    count++;
+    like1.innerText = count + " like(s)";
+    console.log(count);
+}
+
+var count = 9;
+var like2 = document.querySelector("#likes2");
+
+console.log(like);
+
+function addLike2(){
+    count++;
+    like2.innerText = count + " like(s)";
+    console.log(count);
+}
